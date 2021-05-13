65f5befa9561c16beae667a58897bd191b00d23d
Tony Turner <tonyturner1399@gmail.com>
2021-05-12 23:04:22 -0400

    Finished Client Side (Future Creating second Constant Server and add CLIENT to CLIENT Messaging)

--- src/Client.java
+++ src/Client.java
@@ -10,7 +10,7 @@ public class Client extends JFrame{
      private JTextArea chatWindow;
      private ObjectOutputStream output;
      private ObjectInputStream input;
-     private String messsage = "";
+     private String message = "";
      private String serverIP;
      private Socket connection;
 
@@ -29,6 +29,7 @@ public class Client extends JFrame{
 
          add(userText, BorderLayout.NORTH);
          chatWindow = new JTextArea();
+         chatWindow.setEditable(false);
          add(new JScrollPane(chatWindow),BorderLayout.CENTER);
          setSize(600,400);
          setVisible(true);
@@ -40,7 +41,7 @@ public class Client extends JFrame{
              setupStreams();
              whileChatting();
          }catch (EOFException eofException){
-             showMessage("/n Client Terminated Connection");
+             showMessage("\n Client Terminated Connection");
          }catch(IOException ioException){
              ioException.printStackTrace();
          }finally{
@@ -61,6 +62,54 @@ public class Client extends JFrame{
         output = new ObjectOutputStream(connection.getOutputStream());
         output.flush();
         input = new ObjectInputStream(connection.getInputStream());
-        showMessage("/n Streams Setup /n ");
+        showMessage("\n Streams Setup \n ");
     }
+
+    private void whileChatting() throws IOException{
+         ableToType(true);
+         do {
+             try {
+                message = (String) input.readObject();
+                showMessage("\n" + message);
+             }catch (ClassNotFoundException classNotFoundException){
+                 showMessage("\n Unknown Data");
+             }
+
+         }while(!message.equals("Server - End"));
+    }
+
+    private void closeCrap(){
+         showMessage("\n Closing Chat Client");
+         ableToType(false);
+         try {
+             output.close();
+             input.close();
+             connection.close();
+         }catch (IOException ioException){
+             ioException.printStackTrace();
+         }
+    }
+
+    private void sendMessage(String message){
+        try{
+            output.writeObject("Client - " + message);
+            output.flush();
+            showMessage("\n Client -" + message);
+        }catch(IOException ioException){
+            chatWindow.append("\n ERROR: CAN'T SEND MESSAGE ");
+        }
+    }
+
+    private void showMessage(final String message){
+        SwingUtilities.invokeLater(
+                () -> chatWindow.append(message)
+        );
+    }
+    //Give Client Permission to use text box
+    private void ableToType(final boolean bool){
+        SwingUtilities.invokeLater(
+                () -> userText.setEditable(bool)
+        );
+    }
+
 }
